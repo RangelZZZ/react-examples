16f971e671756ec13c1c1df863b4390f8e0f439b
RangelZZZ <18792561236@163.com>
2016-08-04 14:17:58 +0800

    finished props and status

--- js/app.js
+++ js/app.js
@@ -1,1 +1,50 @@
-// TODO
+const App = React.createClass({
+    getInitialState: function () {
+        return {
+            liked: false
+        }
+    },
+    handleOnclick: function () {
+        this.setState({
+            liked: !this.state.liked
+        })
+    },
+
+    render: function () {
+        const text = this.state.liked ? "liked" : "disliked";
+        return <div>
+            <p onClick={this.handleOnclick}>
+                you {text} apple
+            </p>
+        </div>
+    }
+});
+
+
+const Input = React.createClass({
+    getInitialState: function () {
+        return {
+            value: "hello"
+        }
+    },
+    handleOnclick: function (event) {
+        this.setState({
+            value: event.target.value
+        })
+    },
+
+    render: function () {
+        var value = this.state.value;
+        return <div>
+            <input type="text" value={value} onChange={this.handleOnclick}/>
+            <p>{value}</p>
+        </div>
+    }
+});
+
+
+ReactDOM.render(
+    <div>
+        <App/>
+        <Input/>
+    </div>, document.getElementById("content"));
